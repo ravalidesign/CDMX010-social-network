4b38248764b7f1285eca9b002e5e4d87d0bdc95d
anahir21 <an211291@hotmail.com>
2021-03-09 13:07:11 -0600

    merging?

--- src/signUp.js
+++ src/signUp.js
@@ -3,17 +3,18 @@ import { createUser } from "./lib/aut.js";
 
  export const sign = (target) =>{
     const html= `
-<h1 id="title">¡A Pueblear!</h1>
+<h1 id="titulo">¡A Pueblear!</h1>
 <section class="form-register">
 <h4> Regístrate </h4>
 <input class="controls" type="text" name="nombre" id="name" placeholder="Ingrese su Nombre Completo">
 <input class="controls" type="text" name="nombre" id="email" placeholder="Ingrese su Correo">
+<span id ='veryfyMail'></span>
 <input class="controls" type="date" name="date of birth" id="date" placeholder="Ingrese su fecha de nacimiento">
 <input class="controls" type="password" name="nombre" id="password" placeholder="Escriba una Contraseña">
-<button class=" buttonSignUp" type="button" id ="register"> Ingresar</button>
+<button class=" boton" type="button" id ="register"> Ingresar</button>
 <p id="pRegistro">ó registrate con<p>
 
-<div id="socialIcons">
+<div id="redes">
 <button class="btn-r" id="facebook-btn"><img src="../Assets/facebook.png.png"></button>
 <button class="btn-r" id="google-btn"><img src="../Assets/google.png.png"></button>
 <button class="btn-r" id="Twitter-btn"><img src="../Assets/twittericon.png.png"></button>
@@ -33,6 +34,33 @@ btnRegistro.addEventListener("click" ,(e) => {
     console.log(email)
 
 })
+function validarEmail(valor) {
+    
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+     alert("La dirección de email " + valor + " es correcta!.");
+    } else {
+     alert("Escribe bien tu correo, por favor!.");
+    }
+  }
+
+  email.addEventListener('event', function (){
+     const email=document.getElementById("email").value;
+
+    validarEmail(email);
+    
+  
+
+})  
+
+
+
+
 }
+//Función que revisa si el correo está bien escrito//
+
+
+
+
+
 export default sign;
 
