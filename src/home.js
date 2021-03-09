c7b12149d5cae65cf67f63621cdcb379483ec861
katterine-Med <katerinm13@gmail.com>
2021-03-08 21:19:12 -0600

    prueba de merge

--- src/home.js
+++ src/home.js
@@ -3,17 +3,9 @@ import { onNavigate } from './router.js'
 export const home = (target) => {
     const html = `
     <header>
-<<<<<<< HEAD
     
 <nav class= "mainmenu" id= "topbar">
         <label for ="btnmenu"><img src="../assets/burgerbutton.png"></label>
-=======
-
-        <input type="checkbox" id="btnmenu">
-        <label for ="btnmenu"><img src="../assets/burger.png"></label>
-        
-        <nav class="menu">
->>>>>>> 7f3ed8e0e40d23e75c62365c4d0573fcbee9e47a
         <ul>
         <li><a id="mainMenu" href="#">Home</a> </li>
         <li><a id="about" href="#">Acerca de</a> </li>
